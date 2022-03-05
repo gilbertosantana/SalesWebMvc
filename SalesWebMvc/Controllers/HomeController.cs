42a33be035de1884321e1f78220afa6acefe0b71
gilbertosantana <gilbertosantoss307@gmail.com>
2022-03-05 19:02:01 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,6 +20,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
+            ViewData["Email"] = "gilbertosantoss307@gmail.com";
             return View();
         }
 
