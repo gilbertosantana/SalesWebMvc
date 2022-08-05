05a8d75ad0224d1dc2a27ec1077d838c85bca9a6
gilbertosantana <gilberto.santana@esig.com.br>
2022-08-05 10:56:00 -0300

    Revert "App locale, number and date formatting"
    This reverts commit af0a01f6a1a607ee13b9404bb2f1aaeca7cb4ef9.

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -9,17 +8,8 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-
-        [Display(Name = "Birth Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BithDate { get; set; }
-
-        [Display(Name = "Base Salary")]
-        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
